5f4245b94e3325f61cc748173d56b696e8e1ba59
Satish Nikale <113823799+SatishNikale@users.noreply.github.com>
2024-08-01 17:39:47 +0000

    dataTypes completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+console.log("Satish");
+
+let name = "Satish";
+let age = 21;
+let isLoggedIn = false;
+
+// number => ( range 2^ 53 )
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => now not assigned a value
+// symbol => for uniqueness
+
+
+
+// object 
+
+console.log(typeof undefined); // output - undefined
+console.log(typeof null); // output - object
+
+
+
+
+
+
