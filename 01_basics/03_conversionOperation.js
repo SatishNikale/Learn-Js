72a6b03ce27cd2b226cbe97f1f25eebd502a4ebb
Satish Nikale <113823799+SatishNikale@users.noreply.github.com>
2024-08-01 18:16:13 +0000

    type conversion in JS

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "satish";
+
+// console.log(typeOf score)
+// console.log(typeOf (score));
+
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" = > NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// for boolean
+// 1 => true
+// 0 => false 
+// "" => false
+// "satish" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
