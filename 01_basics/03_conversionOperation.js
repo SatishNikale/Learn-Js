6ac2d4afcde6f3e3de202784fc03fa0905368416
Satish Nikale <113823799+SatishNikale@users.noreply.github.com>
2024-08-02 08:53:32 +0000

    some additional conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "satish";
 
-// console.log(typeOf score)
-// console.log(typeOf (score));
+// console.log(typeof score)
+// console.log(typeof (score));
 
 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" = > NaN
@@ -16,7 +16,7 @@ let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // for boolean
 // 1 => true
@@ -28,7 +28,46 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
+// console.log(stringNumber);
 
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// ******************** Operations in JS ************************//
+
+let value = 5;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // give 2^3 which is 8
+// console.log(2/6);
+// console.log(2%3);
+
+
+let str1 = "satish";
+let str2 = " nikale";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 2 + "2");   // 32(which kind of first converted into  it)
+
+// console.log(+true);  // 1
+// console.log(+"");    // 0
+
+// let gameCounter = 10;
+// gameCounter++;
+// ++gameCounter;
+
+// console.log(gameCounter);
+
